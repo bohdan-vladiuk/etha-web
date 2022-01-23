f1470baf640a6818fd87d429414a5372c10950f3
codedreams <codedreams9873@gmail.com>
2022-01-23 10:34:19 -0500

    fixed modal

--- components/ContactUsModal.tsx
+++ components/ContactUsModal.tsx
@@ -289,7 +289,7 @@ export const ContactUsModal: React.FC<ConatctUsModalProps> = (props: ConatctUsMo
                                                         return (
                                                             <div
                                                                 key={val}
-                                                                className="d-flex justify-content-between align-items-center w-100 p-2"
+                                                                className="d-flex justify-content-between align-items-center w-100 py-2"
                                                                 style={{
                                                                     borderBottom: '1px solid #0000000f',
                                                                     width: '80%',
