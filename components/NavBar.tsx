0f3105d23bde71693575c89cc300b44d28cc3fd0
codedreams <codedreams9873@gmail.com>
2022-03-18 00:38:28 -0400

    Invest button working, can refactor later if needed

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,6 +11,7 @@ import { useRouter } from 'next/router';
 import styles from '../styles/NavBar.module.css';
 import Link from 'next/link';
 import { ContactUsModal } from './ContactUsModal';
+import { ButtonBase } from '@mui/material';
 
 const ignorePathnames = [
     '/landing',
@@ -35,45 +36,58 @@ export const NavBar: React.FC = () => {
     return (
         <>
             <div className={`${styles.home_nav_container}`}>
-                <div
-                    className="p-0 m-0 d-flex align-items-center"
-                    onClick={() => {
-                        history.push('/');
-                    }}
-                    style={{ cursor: 'pointer' }}
-                >
-                    <Image
-                        className="p-0 m-0 d-flex align-items-center mr-3"
-                        height={25}
-                        width={38.94}
-                        src="/logo.svg"
-                    />
-                </div>
-                <div
-                    className={`${styles.nav_link} m-0 p-0 mx-4`}
-                    style={{ cursor: 'pointer' }}
-                    onClick={() => {
-                        history.push(`/about-us`);
-                    }}
-                >
-                    About Us
-                </div>
-                {navTitles.map((val, idx) => {
-                    return (
-                        <div
-                            key={`${val}-${idx}`}
-                            className={`${styles.nav_link} m-0 p-0 mx-4`}
-                            style={{ cursor: 'pointer' }}
-                            onClick={() => {
-                                setShowJoinPage(true);
-                                //history.push(`/${val.toLowerCase().split(' ')[0]}`);
-                            }}
-                        >
-                            {val}
-                        </div>
-                    );
-                })}
-            </div>
+                       <div className="p-0 m-0 d-flex align-items-center"
+                        onClick={() => {
+                            history.push('/');
+                        }}
+                        style={{ cursor: 'pointer' }}
+                    >
+                        <Image
+                            className="p-0 m-0 d-flex align-items-center mr-3"
+                            height={25}
+                            width={38.94}
+                            src="/logo.svg"
+                        />
+                    </div>
+                    <div
+                        className={`${styles.nav_link} m-0 p-0 mx-4`}
+                        style={{ cursor: 'pointer' }}
+                        onClick={() => {
+                            history.push(`/about-us`);
+                        }}
+                    >
+                        About Us
+                    </div>
+                    {navTitles.map((val, idx) => {
+                        return (
+                            <div
+                                key={`${val}-${idx}`}
+                                className={`${styles.nav_link} m-0 p-0 mx-4`}
+                                style={{ cursor: 'pointer' }}
+                                onClick={() => {
+                                    setShowJoinPage(true);
+                                    //history.push(`/${val.toLowerCase().split(' ')[0]}`);
+                                }}
+                            >
+                                {val}
+                            </div>
+                        );
+                    })}
+                    
+                    {/* Invest Button @ top right of page */}
+                        <Button variant='primary' 
+                            href="https://wefunder.com/etha.one"
+                            style={{
+                                    borderRadius:"5px",
+                                    height:"35px",
+                                    marginLeft:"auto",
+                                    display:"flex",
+                                    justifyContent:"center",
+                                    alignItems:"center"
+                                    }} 
+                                >Invest {'>'} 
+                            </Button>
+                 </div>
 
             <ContactUsModal show={showJoinPage} onHide={() => setShowJoinPage(false)} />
         </>
